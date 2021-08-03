cb65d9aa756ba74541d59a215dee21a8a1b5cfdc
zhou-ran <Ranzhou1005@gmail.com>
2021-08-03 15:58:05 +0800

    update HTseq for se datasets

--- AlternativeSplice/script/GeneCounts.R
+++ AlternativeSplice/script/GeneCounts.R
@@ -14,19 +14,19 @@ txdb <-
 
 ebg <- exonsBy(txdb, by = "gene")
 
-strandMode_ <- c(
-  "none" = 0,
-  "forward" = 1,
-  "reverse" = 2
-)
+strandMode_ <- c("none" = 0,
+                 "forward" = 1,
+                 "reverse" = 2)
 
 ## bam parser information
 
-flag <- scanBamFlag(isSecondaryAlignment = FALSE,
-                    isNotPassingQualityControls = FALSE,
-                    isUnmappedQuery = FALSE,
-                    isDuplicate = FALSE)
-sbp <- ScanBamParam(flag=flag, mapqFilter = 255)
+flag <- scanBamFlag(
+  isSecondaryAlignment = FALSE,
+  isNotPassingQualityControls = FALSE,
+  isUnmappedQuery = FALSE,
+  isDuplicate = FALSE
+)
+sbp <- ScanBamParam(flag = flag, mapqFilter = 255)
 
 
 ## read sampleinformation
@@ -36,30 +36,49 @@ sampleList <- read.table(config,
                          header = T)
 
 ## here to split into different strand specific mode
-fileNames <- split(sampleList$names, sampleList$Strand)
-se <- lapply(names(fileNames),function(x){
-  
-  name <- fileNames[[x]]
+fileNames <- split(sampleList[, c('type', 'Strand')], sampleList$names)
+se <- lapply(names(fileNames), function(x) {
+  name <- x
+  strandInfo <- unname(unlist(fileNames[[x]])['Strand'])
+  fragType <- unname(unlist(fileNames[[x]])['type'])
   # Must change the search dir or pattern based your output
   bamFile <- paste('./data/STAR/',
-                 name,
-                 '.Aligned.sortedByCoord.out.bam',
-                 sep = '')
-
+                   name,
+                   '.Aligned.sortedByCoord.out.bam',
+                   sep = '')
+  
   bamFile <- BamFileList(bamFile,
-                        yieldSize = 2000000)
-
-
-  se<- summarizeOverlaps(features = ebg, 
-                                reads = bamFile, 
-                                mode = "Union",
-                                ignore.strand = FALSE, 
-                                inter.feature = FALSE, 
-                                singleEnd = FALSE,
-                                fragments = FALSE, 
-                                strandMode = unname(strandMode_[x]), 
-                                param = sbp, 
-                                preprocess.reads = NULL)
+                         yieldSize = 2000000)
+  
+  if (fragType == 'SE') {
+    se <- summarizeOverlaps(
+      features = ebg,
+      reads = bamFile,
+      mode = "Union",
+      ignore.strand = TRUE,
+      inter.feature = ifelse(strandInfo == 'none', TRUE, FALSE),
+      singleEnd = TRUE,
+      fragments = FALSE,
+      param = sbp,
+      preprocess.reads = NULL
+    )
+    else {
+      se <- summarizeOverlaps(
+        features = ebg,
+        reads = bamFile,
+        mode = "Union",
+        ignore.strand = FALSE,
+        inter.feature = FALSE,
+        singleEnd = FALSE,
+        fragments = FALSE,
+        strandMode = unname(strandMode_[strandInfo]),
+        param = sbp,
+        preprocess.reads = NULL
+      )
+      
+    }
+    
+  }
   se
 })
 
--- RNASeq/script/GeneCounts.R
+++ RNASeq/script/GeneCounts.R
@@ -36,9 +36,11 @@ sampleList <- read.table(config,
                          header = T)
 
 ## here to split into different strand specific mode
-fileNames <- split(sampleList$names, sampleList$Strand)
+fileNames <- split(sampleList[, c('type', 'Strand')], sampleList$names)
 se <- lapply(names(fileNames), function(x) {
-  name <- fileNames[[x]]
+  name <- x
+  strandInfo <- unname(unlist(fileNames[[x]])['Strand'])
+  fragType <- unname(unlist(fileNames[[x]])['type'])
   # Must change the search dir or pattern based your output
   bamFile <- paste('./data/STAR/',
                    name,
@@ -48,19 +50,35 @@ se <- lapply(names(fileNames), function(x) {
   bamFile <- BamFileList(bamFile,
                          yieldSize = 2000000)
   
-  
-  se <- summarizeOverlaps(
-    features = ebg,
-    reads = bamFile,
-    mode = "Union",
-    ignore.strand = FALSE,
-    inter.feature = FALSE,
-    singleEnd = FALSE,
-    fragments = FALSE,
-    strandMode = unname(strandMode_[x]),
-    param = sbp,
-    preprocess.reads = NULL
-  )
+  if (fragType == 'SE') {
+    se <- summarizeOverlaps(
+      features = ebg,
+      reads = bamFile,
+      mode = "Union",
+      ignore.strand = TRUE,
+      inter.feature = ifelse(strandInfo == 'none', TRUE, FALSE),
+      singleEnd = TRUE,
+      fragments = FALSE,
+      param = sbp,
+      preprocess.reads = NULL
+    )
+    else {
+      se <- summarizeOverlaps(
+        features = ebg,
+        reads = bamFile,
+        mode = "Union",
+        ignore.strand = FALSE,
+        inter.feature = FALSE,
+        singleEnd = FALSE,
+        fragments = FALSE,
+        strandMode = unname(strandMode_[strandInfo]),
+        param = sbp,
+        preprocess.reads = NULL
+      )
+      
+    }
+    
+  }
   se
 })
 
