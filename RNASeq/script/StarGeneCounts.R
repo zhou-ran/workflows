a73363503056f9034c3d14adf946b5e073479978
zhou-ran <Ranzhou1005@gmail.com>
2021-08-09 12:42:48 +0800

    add gene quantification results from STAR

---
+++ RNASeq/script/StarGeneCounts.R
@@ -0,0 +1,42 @@
+args = commandArgs(T)
+config = args[1]
+output = args[2]
+output_dir = args[3]
+
+
+## read sampleinformation
+sampleList <- read.table(config,
+                         stringsAsFactors = F,
+                         sep = "\t",
+                         header = T)
+
+
+
+processReadsFile <- function(fullFileName) {
+  dat <- read.table(fullFileName, stringsAsFactors = T)
+  dat <-
+    data.frame(dat[, -1], row.names = dat[, 1])
+  return(dat[-c(1:4),])
+}
+
+getGivenCol <- function(listOfDf, colId) {
+  do.call(cbind, lapply(listOfDf, "[", colId)) -> res
+  colnames(res) <- names(listOfDf)
+  res
+}
+
+
+fullFileName <- file.path(star_output_dir,
+                          paste0(sampleList$names, ".ReadsPerGene.out.tab"))
+names(fullFileName) <- sampleList$names
+
+lapply(fullFileName, processReadsFile) -> res
+
+dat <- list(
+  "none" = getGivenCol(res, "V2"),
+  "forward" = getGivenCol(res, "V3"),
+  "reverse" = getGivenCol(res, "V4")
+)
+
+saveRDS(dat,
+        file = output)
